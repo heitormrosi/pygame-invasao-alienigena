78e8af9160fd4289a24bc5a1d8be0c57e4e7fb2e
heitormrosi <heitor2509@outlook.com>
2026-02-12 20:50:31 -0300

    Nave atira e código refatorado, finalização do capítulo 12 do livro.

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 def run_game() -> None:
     pygame.init()
@@ -17,13 +18,19 @@ def run_game() -> None:
     
     ship = Ship(screen, ai_settings)
     
+    bullets = Group()
+    
     while True:
-        gf.check_events(ship)
-
+        gf.check_events(ai_settings, screen, ship, bullets)
+        
         # Atualiza o movimento da espaçonave
         ship.update()
+         
+        bullets.update()
+        
+        gf.update_bullets(bullets)
 
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_screen(ai_settings, screen, ship, bullets)
     
 
 if __name__ == "__main__":
--- game_functions.py
+++ game_functions.py
@@ -3,22 +3,48 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from pygame.sprite import Group
+from bullet import Bullet
 
-def check_events(ship: Ship) -> None:
+def check_events(
+        ai_settings: Settings, 
+        screen: pygame.Surface, 
+        ship: Ship, 
+        bullets: Group
+        ) -> None:
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                check_keydown_events(event, ship)
+                check_keydown_events(
+                    event, 
+                    ai_settings,
+                    screen,
+                    ship,
+                    bullets
+                )
             elif event.type == pygame.KEYUP:
                 check_keyup_events(event, ship)
 
 
-def check_keydown_events(event: pygame.event.Event, ship: Ship) -> None:
+def check_keydown_events(
+        event: pygame.event.Event, 
+        ai_settings: Settings, 
+        screen: pygame.Surface, 
+        ship: Ship, 
+        bullets: Group
+        ) -> None:
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     if event.key == pygame.K_LEFT:
         ship.moving_left = True
+    if event.key == pygame.K_SPACE:
+        fire_bullet(
+            ai_settings,
+            screen,
+            ship,
+            bullets
+        )
 
 
 def check_keyup_events(event: pygame.event.Event, ship: Ship) -> None:
@@ -29,11 +55,44 @@ def check_keyup_events(event: pygame.event.Event, ship: Ship) -> None:
 
 
 def update_screen(
-          ai_settings: Settings, screen: pygame.Surface, ship: Ship) -> None:
+          ai_settings: Settings, 
+          screen: pygame.Surface, 
+          ship: Ship, 
+          bullets: Group
+          ) -> None:
     screen.fill(ai_settings.bg_color)
-        
+    
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+
     # Renderiza a espaçonave na tela
     ship.blitme()
     
     # Atualiza a renderização da janela
     pygame.display.flip()
+
+def update_bullets(bullets):
+    """ Atualiza a posição dos projéteis e se livra dos projéteis antigos. """
+
+    # Atualiza as posições dos projéteis 
+    bullets.update()
+    # Livra-se dos projéteis que desapareceram 
+    for bullet in bullets.copy(): 
+        if bullet.rect.bottom <= 0: 
+            bullets.remove(bullet)
+
+
+def fire_bullet(
+        ai_settings: Settings,
+        screen: pygame.Surface,
+        ship: Ship,
+        bullets: Group
+        ) -> None:
+    if len(bullets) < ai_settings.bullets_allowed:
+        bullets.add(
+            Bullet(
+                ai_settings,
+                screen,
+                ship
+            )
+        )
